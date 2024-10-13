1491aeaf0f23504b39f2d33bdad222325196f719
NitchayaninT <146054441+NitchayaninT@users.noreply.github.com>
2024-10-13 23:24:37 +0700

    Update print solution

--- src/nqueen/NQueen.java
+++ src/nqueen/NQueen.java
@@ -1,18 +1,25 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package nqueen;
-
+import java.util.*;
 /**
  *
  * @author panupongsangaphunchai
  */
 public class NQueen {
+    int N; //size of board
 
-    /**
-     * @param args the command line arguments
-     */
+    public void printSolution(int board[][])
+    {
+        System.out.printf("%8s 1  2  3  4"," ");
+        for(int i=0;i<N;i++)
+        {
+            System.out.printf("row %d | ",N);
+            for(int j=0;j<N;j++)
+            {
+                System.out.print(" " + board[i][j]+ " ");
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
         System.out.println("nQueen!!!");
     }
